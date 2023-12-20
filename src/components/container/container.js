0172112036f98830b4b14c68073c5e0b2445ac9a
Tejas Gawande <tejugawande25@gmail.com>
2023-12-21 00:19:01 +0530

    adds dashboard container

---
+++ src/components/container/container.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./container.css";
+
+function Container(){
+    return(
+        <>
+        <div className="container">
+            <img src="https://images.pexels.com/photos/3278215/pexels-photo-3278215.jpeg?cs=srgb&dl=pexels-oleksandr-p-3278215.jpg&fm=jpg" alt="image"></img>
+            <h1>Travel the world!</h1>
+            <p>Exploring the world and taking the new experiences.</p>
+        </div>
+        </>
+    )
+}
+export default Container;
