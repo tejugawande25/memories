5f32a6c657ae31673a0051e710be89a740b826ac
Tejas Gawande <tejugawande25@gmail.com>
2023-12-21 16:54:01 +0530

    dashboard

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -7,6 +7,10 @@ function Container(){
         <div className="dash-container">
             <div className="card">
             <img src="https://images.pexels.com/photos/3278215/pexels-photo-3278215.jpeg?cs=srgb&dl=pexels-oleksandr-p-3278215.jpg&fm=jpg"></img>
+            <div>
+            <h1>Travel</h1>
+            <p>Create memmories that lasts lifetime.</p>
+            </div>
             </div>
         </div>   
 
