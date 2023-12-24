d29f0662b9c155ac4c09a85a83d86903b7523f89
Tejas Gawande <tejugawande25@gmail.com>
2023-12-24 23:55:50 +0530

    add nav bar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,6 +5,11 @@ function Header() {
   return (
     <div className="header">
       <h1>Memories</h1>
+      <nav className="navbar">
+        <ul className="tabs" style={{listStyle:"none"}}>
+          <li><a href="card" style={{textDecoration:"none"}}>Home</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
